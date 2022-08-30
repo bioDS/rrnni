72c892dcc28a4722c3ba954ce882da64ae7483bd
Jiří Moravec <jiri.c.moravec@gmail.com>
2022-08-30 17:02:52 +1200

    Added check to test for malformed branch lengths

--- R/validate_rankedPhylo.r
+++ R/validate_rankedPhylo.r
@@ -25,24 +25,27 @@ validate_rankedPhylo = function(x){
         stop("x must inherit from \"phylo\"")
 
     if(!identical(names(x), c("edge", "tip.label", "Nnode", "edge.length", "rank")))
-        stop("x names do not correspond to names of \"rankedPhylo\" object")
+        stop("x's names do not correspond to names of \"rankedPhylo\" object")
 
     if(!identical(sapply(x, typeof) |> unname(),
                   c("integer", "character", "integer", "integer", "integer"))
         )
-        stop("x types do not correspond to types of \"rankedPhylo\" object")
+        stop("x's types do not correspond to types of \"rankedPhylo\" object")
 
     if(!ape::is.rooted(x))
         stop("x must be rooted")
 
     if(!identical(c(rep.int(0L, ape::Ntip(x)), seq_len(x$Nnode)), sort(x$rank)))
-        stop("x ranks are malformed")
+        stop("x's ranks are malformed")
 
     if((ape::Ntip(x) + 1) %in% x$edge[,2])
         stop("Node ", ape::Ntip(x) + 1, " is not the root node.")
 
     if(!identical(dim(x$edge), c(ape::Ntip(x)*2L - 2L, 2L)))
-        stop("x edge matrix does not have correct dimension.")
+        stop("x's edge matrix does not have correct dimension.")
+
+    if(any(ape::node.depth.edgelength(x) < 0))
+        stop("x's edge.length is malformed.")
 
     invisible(NULL)
     }

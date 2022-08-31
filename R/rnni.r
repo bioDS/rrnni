aeb08449118814d93ea0aacffe3f6f6186ecf023
Jiří Moravec <jiri.c.moravec@gmail.com>
2022-08-31 16:12:38 +1200

    Handle trees with different tip labels + test

--- R/rnni.r
+++ R/rnni.r
@@ -5,6 +5,11 @@
 #' @return an integer RNNI distance between `x` and `y`
 #'
 #' @export
-rnni = function(x, y){
-    return(.Call(C_rnni_distance, x, y))
+rnni = function(x, y, normalize=FALSE, verbose=FALSE){
+    trees = c(x,y)
+    tips = common_tips(trees)
+    trees = keep_tips(trees, tips)
+    trees = sort_tips(trees)
+
+    return(.Call(C_rnni_distance, trees[[1]], trees[[2]]))
     }
--- tests/testthat/test-rnni.r
+++ tests/testthat/test-rnni.r
@@ -2,3 +2,10 @@ test_that("rnni distance for the same tree", {
     tree = rankedPhylo(5)
     expect_identical(rnni(tree, tree), 0L)
     })
+
+
+test_that("rnni distance for different trees", {
+    x = read_newick("((((A:1,B:1):1,C:2):1,D:3):1,E:4);")
+    y = read_newick("(((A:1,D:1):2,(C:2,E:2):1):1,B:4);")
+    expect_identical(rnni(x, y), 5L)
+    })
